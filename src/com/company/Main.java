3957d8a0493e9cdeb335741700d0ddce3fb08de0
Omar <s205471@outook.dk>
2020-09-28 15:24:44 +0200

    mit comit omar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("ayy");
         System.out.println("hello world");
         System.out.println(hello world);
+        System.out.println("hej");
     }
 }
 
